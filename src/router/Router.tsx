cc46a41bbae4156421d7694268e9e145f660b39a
motohiro takagi <tkg.8.mthr@gmail.com>
2021-12-19 17:36:47 +0900

    ヘッダーを作成

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -6,14 +6,18 @@ import { Home } from "../components/pages/Home"
 import { Setting } from "../components/pages/Setting"
 import { UserManagement } from "../components/pages/UserManagement"
 import { Page404 } from "../components/pages/Page404"
+import { HeaderLayout } from "../components/templates/HeaderLayout"
 
 export const Router: VFC = memo(() => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/home/setting" element={<Setting />} />
-      <Route path="/home/user_management" element={<UserManagement />} />
+        <Route path="/home"
+          element={<HeaderLayout><Home /></HeaderLayout>} />
+        <Route path="/home/setting"
+          element={<HeaderLayout><Setting /></HeaderLayout>} />
+        <Route path="/home/user_management"
+          element={<HeaderLayout><UserManagement /></HeaderLayout>} />
       <Route path="*" element={<Page404 />} />
     </Routes>
   )
