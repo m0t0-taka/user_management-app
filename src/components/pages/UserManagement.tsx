5129cfd2254971248c826999b52480fd062fdf53
motohiro takagi <tkg.8.mthr@gmail.com>
2021-12-20 23:37:18 +0900

    ユーザー一覧画面作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,18 @@
 import { memo, VFC } from "react"
+import { Wrap, WrapItem } from '@chakra-ui/react'
+
+import { UserCard } from "../organisms/user/UserCard"
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページ</p>
+  return (
+    <Wrap p={{ base:4, md:10 }}>
+        <WrapItem>
+          <UserCard
+            imageUrl="http://source.unsplash.com/random"
+            userName="もと"
+            fullName="takagi motohiro"
+          />
+        </WrapItem>
+    </Wrap>
+  )
 })
