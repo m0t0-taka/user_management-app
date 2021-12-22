e9988d50fc67c15105d97b5e44276336d084f134
motohiro takagi <tkg.8.mthr@gmail.com>
2021-12-22 23:46:01 +0900

    データ取得中はローディングアイコンが表示されるようにした

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,18 +1,31 @@
-import { memo, VFC } from "react"
-import { Wrap, WrapItem } from '@chakra-ui/react'
+import { memo, useEffect, VFC } from "react"
+import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react'
 
 import { UserCard } from "../organisms/user/UserCard"
+import { useAllUsers } from "../../hooks/useAllUsers"
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers()
+
+  useEffect(() => getUsers(), [])
+
   return (
-    <Wrap p={{ base:4, md:10 }}>
-        <WrapItem>
-          <UserCard
-            imageUrl="http://source.unsplash.com/random"
-            userName="もと"
-            fullName="takagi motohiro"
-          />
-        </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+      <Wrap p={{ base:4, md:10 }}>
+          <WrapItem>
+            <UserCard
+              imageUrl="http://source.unsplash.com/random"
+              userName="もと"
+              fullName="takagi motohiro"
+            />
+          </WrapItem>
+      </Wrap>
+      )}
+    </>
   )
 })
